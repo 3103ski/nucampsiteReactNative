6e2a05b71e3e79919e4d02816cc020d795f8177b
3103ski <bjastski@gmail.com>
2020-12-13 11:35:40 -0800

    adds email messaging from contact page

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 
 // Third Party
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class ContactComponent extends Component {
 	static navigationOptions = {
 		title: 'Contact Us',
 	};
+
+	sendMail() {
+		MailComposer.composeAsync({
+			recipients: ['campsites@nucamp.co'],
+			subject: 'inquiry',
+			body: 'to whom it may concern',
+		});
+	}
 
 	render() {
 		return (
@@ -21,6 +30,12 @@ class ContactComponent extends Component {
 						<Text style={{ marginBottom: 10 }}>U.S.A.</Text>
 						<Text>Phone: 1-206-555-1234</Text>
 						<Text>Email: campsites@nucamp.co</Text>
+						<Button
+							title='Send Email'
+							buttonStyle={{ backgroundColor: '#5637dd', margin: 40 }}
+							icon={<Icon name='envelope-o' type='font-awesome' color='#fff' iconStyle={{ marginRight: 10 }} />}
+							onPress={() => this.sendMail()}
+						/>
 					</Card>
 				</Animatable.View>
 			</ScrollView>
